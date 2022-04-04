2ce4e15fa9fc830a42573ad714de9895df63f518
Vladimir Merkulov <merkulov-vladimir93@mail.ru>
2022-04-04 21:51:25 +0700

    tests corrected

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -32,4 +32,26 @@ class TicketManagerTest {
         Ticket[] actual = manager.findAll("VNK", "RGA");
         assertArrayEquals(actual, expected);
     }
+    @Test                           // один билет верный
+    void findAllWhenOneCorrect() {
+        repository.save(one);
+        repository.save(two); // must be 4, price 500
+        repository.save(three);
+        repository.save(fife);
+        repository.save(seven);
+        Ticket[] expected = {two};
+        Ticket[] actual = manager.findAll("VNK", "RGA");
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test                           //ожидаемый набор пустой
+    void findAllWhenEmpty() {
+        repository.save(one);
+        repository.save(three);
+        repository.save(fife);
+        repository.save(seven);
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("VNK", "RGA");
+        assertArrayEquals(actual, expected);
+    }
 }
